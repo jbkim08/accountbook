a6c446e7d6b8b77851cd23fa73d5f1804c8ccc5b
KJB <drv98@naver.com>
2026-07-24 11:28:53 +0900

    컨트롤러

---
+++ src/main/java/com/example/accountbook/controller/TransactionController.java
@@ -0,0 +1,34 @@
+package com.example.accountbook.controller;
+
+import com.example.accountbook.model.Transaction;
+import com.example.accountbook.service.TransactionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class TransactionController {
+
+    private final TransactionService transactionService;
+
+    // 가계부 메인 페이지 (목록 조회)
+    @GetMapping("/")
+    public String index(Model model) {
+        //Model 객체는 View에 데이터 전달을 위한 객체(request)
+        List<Transaction> transactions = transactionService.getAllTransactions();
+        model.addAttribute("transactions", transactions);
+        return "index";
+    }
+
+    // 새 트랜젝션 저장 처리
+    @PostMapping("/add")
+    public String addTransaction(Transaction transaction) {
+        transactionService.addTransaction(transaction); //db에 저장됨
+        return "redirect:/"; // 저장 후 메인 페이지로 리다이렉트
+    }
+}
