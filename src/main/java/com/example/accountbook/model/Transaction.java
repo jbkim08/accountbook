2f3d47cf168f05593fbaecb8ce4041fb6bebcfed
KJB <drv98@naver.com>
2026-07-24 12:34:53 +0900

    유효성검사 항목

--- src/main/java/com/example/accountbook/model/Transaction.java
+++ src/main/java/com/example/accountbook/model/Transaction.java
@@ -1,5 +1,6 @@
 package com.example.accountbook.model;
 
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,9 +9,19 @@ import java.time.LocalDateTime;
 @Getter @Setter
 public class Transaction {
     private Long id;
+
+    @NotBlank(message = "내역을 입력해주세요.")
+    @Size(max = 50, message = "내역은 50자 이내로 입력해주세요.")
     private String title;
+
+    @NotNull(message = "금액을 입력해주세요.")
+    @Min(value = 1, message = "금액은 1원 이상이어야 합니다.")
+    @Max(value = 1000000000, message = "금액이 너무 큽니다.")
     private Long amount;
+
+    @NotBlank(message = "유형을 선택해 주세요")
     private String type;     // INCOME, EXPENSE
+
     private String category;
     private LocalDateTime regDate;
 }
