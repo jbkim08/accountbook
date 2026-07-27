cee12b351635a5ee02fcf5d4e5bd0028f317a342
KJB <drv98@naver.com>
2026-07-27 15:58:45 +0900

    UserDetails 대신 User

--- src/main/java/com/example/accountbook/controller/TransactionController.java
+++ src/main/java/com/example/accountbook/controller/TransactionController.java
@@ -5,7 +5,7 @@ import com.example.accountbook.service.TransactionService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -27,40 +27,42 @@ public class TransactionController {
             @RequestParam(required = false) String startDate,
             @RequestParam(required = false) String endDate,
             @RequestParam(required = false) String category,
-            Model model, Transaction transaction, @AuthenticationPrincipal UserDetails userDetails) {
+            Model model, Transaction transaction,
+            @AuthenticationPrincipal User user) {
         int pageSize = 10; //한페이지에 10개 표시
+        String username = user.getUsername();
         //검색조건이 포함된 목록과 전체 개수 조회
-        model.addAttribute("transactions", transactionService.getTransactions(page, pageSize, startDate, endDate, category, userDetails.getUsername()));
-        int totalPages = transactionService.getTotalPages(pageSize, startDate, endDate, category, userDetails.getUsername());
+        model.addAttribute("transactions", transactionService.getTransactions(page, pageSize, startDate, endDate, category, username));
+        int totalPages = transactionService.getTotalPages(pageSize, startDate, endDate, category, username);
         model.addAttribute("currentPage", page);
         model.addAttribute("totalPages", totalPages);
         model.addAttribute("startDate", startDate);
         model.addAttribute("endDate", endDate);
         model.addAttribute("category", category);
         // 합계 로직
-        model.addAttribute("totalIncome", transactionService.getTotalIncome(userDetails.getUsername()));
-        model.addAttribute("totalExpense", transactionService.getTotalExpense(userDetails.getUsername()));
+        model.addAttribute("totalIncome", transactionService.getTotalIncome(username));
+        model.addAttribute("totalExpense", transactionService.getTotalExpense(username));
         return "index";
     }
     // id로 해당 트랜젝션을 삭제한다.
     @GetMapping("/delete/{id}")
-    public String deleteTransaction(@PathVariable Long id, @AuthenticationPrincipal UserDetails userDetails) {
-        transactionService.removeTransaction(id, userDetails.getUsername());
+    public String deleteTransaction(@PathVariable Long id, @AuthenticationPrincipal User user) {
+        transactionService.removeTransaction(id, user.getUsername());
         return "redirect:/";
     }
     // 새 트랜젝션 저장 처리
     @PostMapping("/add")
     public String addTransaction(@Valid Transaction transaction,
                                  BindingResult bindingResult, Model model,
-                                 @AuthenticationPrincipal UserDetails userDetails) {
+                                 @AuthenticationPrincipal User user) {
         if (bindingResult.hasErrors()) {
             model.addAttribute("currentPage", 1);
-            model.addAttribute("transactions", transactionService.getAllTransactions(userDetails.getUsername()));
-            model.addAttribute("totalIncome", transactionService.getTotalIncome(userDetails.getUsername()));
-            model.addAttribute("totalExpense", transactionService.getTotalExpense(userDetails.getUsername()));
+            model.addAttribute("transactions", transactionService.getAllTransactions(user.getUsername()));
+            model.addAttribute("totalIncome", transactionService.getTotalIncome(user.getUsername()));
+            model.addAttribute("totalExpense", transactionService.getTotalExpense(user.getUsername()));
             return "index"; //되돌아 가기
         }
-        transaction.setUsername(userDetails.getUsername()); //유저네임을 추가한다.
+        transaction.setUsername(user.getUsername()); //유저네임을 추가한다.
         transactionService.addTransaction(transaction); //db에 저장됨
         return "redirect:/"; // 저장 후 메인 페이지로 리다이렉트
     }
