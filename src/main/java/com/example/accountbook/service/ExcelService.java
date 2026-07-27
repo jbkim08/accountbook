7bdceeaccbf32fc0e47bf57a4c9329c573ef7dd4
KJB <drv98@naver.com>
2026-07-27 16:11:24 +0900

    엑셀에 유저네임 추가

--- src/main/java/com/example/accountbook/service/ExcelService.java
+++ src/main/java/com/example/accountbook/service/ExcelService.java
@@ -15,7 +15,7 @@ public class ExcelService {
 
     //트랜젝션 리스트를 입력받아서 엑셀로 변환(쓰기) => 다운로드가 가능하도록 입력스트림으로 리턴
     public ByteArrayInputStream transactionsToExcel(List<Transaction> transactions) throws IOException {
-        String[] columns = {"ID", "내역", "금액", "유형", "카테고리", "등록일"};
+        String[] columns = {"ID", "내역", "금액", "유형", "카테고리", "등록일", "유저네임"};
 
         try (Workbook workbook = new XSSFWorkbook(); ByteArrayOutputStream out = new ByteArrayOutputStream()) {
             Sheet sheet = workbook.createSheet("가계부 내역"); //엑셀시트 제목
@@ -44,6 +44,7 @@ public class ExcelService {
                 row.createCell(3).setCellValue(t.getType());
                 row.createCell(4).setCellValue(t.getCategory());
                 row.createCell(5).setCellValue(t.getRegDate().toString());
+                row.createCell(6).setCellValue(t.getUsername());
             }
 
             workbook.write(out);
